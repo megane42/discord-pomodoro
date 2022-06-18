028cab9f6407db85aca5fea359e9591e72d807db
megane42 <h.kzm.gm@gmail.com>
2022-06-18 18:40:25 +0900

    initial commit
    $ bundle gem discord-pomodoro --exe --mit --test=rspec
    
    Creating gem 'discord-pomodoro'...
    rspec is already configured, ignoring --test flag.
    MIT License enabled in config
    Changelog enabled in config
    RuboCop enabled in config
    Initializing git repo in /home/megane42/src/megane42/discord-pomodoro
          create  discord-pomodoro/Gemfile
    
          create  discord-pomodoro/lib/discord/pomodoro.rb
          create  discord-pomodoro/lib/discord/pomodoro/version.rb
          create  discord-pomodoro/discord-pomodoro.gemspec
          create  discord-pomodoro/Rakefile
          create  discord-pomodoro/README.md
          create  discord-pomodoro/bin/console
          create  discord-pomodoro/bin/setup
          create  discord-pomodoro/.gitignore
          create  discord-pomodoro/.rspec
          create  discord-pomodoro/spec/spec_helper.rb
          create  discord-pomodoro/spec/discord/pomodoro_spec.rb
          create  discord-pomodoro/LICENSE.txt
          create  discord-pomodoro/CHANGELOG.md
          create  discord-pomodoro/.rubocop.yml
          create  discord-pomodoro/exe/discord-pomodoro

---
+++ spec/discord/pomodoro_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Discord::Pomodoro do
+  it "has a version number" do
+    expect(Discord::Pomodoro::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
