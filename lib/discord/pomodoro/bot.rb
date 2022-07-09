9cc151d4905a4953c23e9256a5f60183ee7ce663
megane42 <h.kzm.gm@gmail.com>
2022-07-10 03:15:49 +0900

    implement periodic job

--- lib/discord/pomodoro/bot.rb
+++ lib/discord/pomodoro/bot.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "discordrb"
+require "rufus-scheduler"
 
 module Discord
   module Pomodoro
@@ -10,10 +11,10 @@ module Discord
         bot.run(true)
         at_exit { bot.stop }
 
-        loop do
-          voice_bot.play_file chime_path
-          sleep 10
-        end
+        connect_voice_bot
+        schedule_chime
+
+        bot.join
       end
 
       private
@@ -26,8 +27,21 @@ module Discord
         @voice_bot ||= bot.voice_connect(ENV["DISCORD_VOICE_CHANNEL_ID"])
       end
 
+      def connect_voice_bot
+        voice_bot
+      end
+
       def chime_path
-        File.expand_path("../../../sounds/chime.mp3", __dir__)
+        @chime_path ||= File.expand_path("../../../sounds/chime.mp3", __dir__)
+      end
+
+      def scheduler
+        @scheduler ||= Rufus::Scheduler.new
+      end
+
+      def schedule_chime
+        scheduler.cron("0 * * * *")  { voice_bot.play_file chime_path }
+        scheduler.cron("50 * * * *") { voice_bot.play_file chime_path }
       end
     end
   end
